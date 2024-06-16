ead03dacca6c1329ccf4580a1fc5e3a5f91d75ea
somebody <somebody@mail.ru>
2024-06-16 15:40:56 +0300

    fix db

--- conf/settings/database.py
+++ conf/settings/database.py
@@ -1,10 +1,10 @@
 from conf.settings.django import env, BASE_DIR
 
-# DATABASES = {"default": env.db()}
+DATABASES = {"default": env.db()}
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
